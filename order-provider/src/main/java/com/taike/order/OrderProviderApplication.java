3d11cd3fa039d43183d3c6a9b2b456b744973f31
yangwen <yangwen@pdnews.cn>
2019-11-04 17:08:53 +0800

    模块优化

--- order-provider/src/main/java/com/taike/order/OrderProviderApplication.java
+++ order-provider/src/main/java/com/taike/order/OrderProviderApplication.java
@@ -3,8 +3,10 @@ package com.taike.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrix
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.taike.**")
