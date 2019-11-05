f4990909413597a8b76a7a1493a3c6c605cf1bd0
yangwen <yangwen@pdnews.cn>
2019-11-05 21:51:17 +0800

    测试nacos

--- order-comsumer/src/main/java/com/taike/order/OrderComsumerApplication.java
+++ order-comsumer/src/main/java/com/taike/order/OrderComsumerApplication.java
@@ -1,15 +1,20 @@
 package com.taike.order;
 
+import com.netflix.discovery.converters.Auto;
 import com.taike.order.config.AvoidTag;
 import com.taike.order.config.RibbonSpecialConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //@RibbonClients(value = {
 //		@RibbonClient(name = "order-provider", configuration = TestConfiguration.class),
@@ -24,10 +29,19 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(value = "com.taike.**", excludeFilters =
         {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {AvoidTag.class})})
 @SpringBootApplication
-
+@RestController
 public class OrderComsumerApplication {
+
+    @Autowired
+    public String currentEnv;
+
     public static void main(String[] args) {
         SpringApplication.run(OrderComsumerApplication.class, args);
+    }
+
+    @GetMapping
+    public String getCurrentEnv(){
+        return currentEnv;
     }
 
 }
--- order-provider/src/main/java/com/taike/order/OrderProviderApplication.java
+++ order-provider/src/main/java/com/taike/order/OrderProviderApplication.java
@@ -1,18 +1,37 @@
 package com.taike.order;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableHystrix
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.taike.**")
+@NacosPropertySource(dataId = "order-provider", autoRefreshed = true)
+@RestController
 public class OrderProviderApplication {
+
+    @NacosValue(value = "${useLocalCache}", autoRefreshed = true)
+    public boolean cache;
+
+
     public static void main(String[] args) {
         SpringApplication.run(OrderProviderApplication.class, args);
+    }
+
+    @GetMapping(value = "fresh")
+    public boolean test() {
+        return cache;
     }
 
 }
