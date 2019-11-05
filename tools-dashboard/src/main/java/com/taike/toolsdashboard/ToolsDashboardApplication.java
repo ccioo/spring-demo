cf59c81774d5933126e9cc5223059adce5d4026b
yangwen <yangwen@pdnews.cn>
2019-11-05 10:55:35 +0800

    添加新模块

---
+++ tools-dashboard/src/main/java/com/taike/toolsdashboard/ToolsDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.taike.toolsdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsDashboardApplication.class, args);
+    }
+
+}
