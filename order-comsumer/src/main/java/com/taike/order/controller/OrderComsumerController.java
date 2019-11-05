e49f2103822f4f5e58ddfc84427562f42b520246
yangwen <yangwen@pdnews.cn>
2019-11-05 11:58:46 +0800

    模块优化

--- order-comsumer/src/main/java/com/taike/order/controller/OrderComsumerController.java
+++ order-comsumer/src/main/java/com/taike/order/controller/OrderComsumerController.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.taike.order.iservice.IOrderPayService;
 import com.taike.order.vo.AlipayVO;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,19 +18,19 @@ public class OrderComsumerController {
 
     @HystrixCommand(fallbackMethod = "defaultError")
     @RequestMapping(value = "/alipay")
-    public String alipay(@ApiParam(name = "用户",
-            value = "传入json格式", required = true)
-                         @RequestBody AlipayVO alipayVO) {
-        if(alipayVO == null){
+    public String alipay(@ApiParam(name = "用户", value = "传入json格式", required = true) @RequestBody AlipayVO alipayVO) {
+        if(StringUtils.isBlank(alipayVO.getOrderId())){
             throw new RuntimeException();
         }
         return orderPayService1.alipay(alipayVO);
     }
 
+
     @RequestMapping(value = "/weixinpay")
     public String weixin() {
         return orderPayService1.weixinpay();
     }
+
 
     public String defaultError(AlipayVO alipayVO) {
         return "error";
