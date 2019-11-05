ca2bf7178fa902e5df3cac85f53eafe3b2044ac8
yangwen <yangwen@pdnews.cn>
2019-11-05 15:03:39 +0800

    [性能优化](1.1.3): 测试提交模板
    测试提交模板

--- order-comsumer/src/main/java/com/taike/order/OrderComsumerApplication.java
+++ order-comsumer/src/main/java/com/taike/order/OrderComsumerApplication.java
@@ -24,6 +24,7 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(value = "com.taike.**", excludeFilters =
         {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {AvoidTag.class})})
 @SpringBootApplication
+
 public class OrderComsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderComsumerApplication.class, args);
