031862dd00b9a32dd3cef9143661c5ed927252ab
yangwen <yangwen@pdnews.cn>
2019-11-07 15:59:10 +0800

    新增自动配置

---
+++ autoconfiguration/src/test/java/com/taike/autoconfiguration/AutoconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taike.autoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoconfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
